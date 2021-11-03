c152c0398dc576317c41d970920560cd9b398376
Andrew Busel <buselax@gmail.com>
2021-11-03 09:44:42 -0400

    commit tree 8a03ca483ca2da68ff00270139c232e649ea2787

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Comment({user, text, removeComment}) {
+    const [likes, setLikes] = useState(0)
+    const [dislikes, setDislikes] = useState(0)
     return (
         <div>
             <h4>{user}</h4>
             <p>{text}</p>
+            <div>
+                <button onClick={()=>{
+                    setLikes((likes)=>likes + 1)
+                }}>{likes}👍</button>
+                <button onClick={()=>{
+                    setDislikes((dislikes)=> dislikes+1)
+                }}>{dislikes}👎</button>
+            </div>
             <button onClick={()=> {
                 removeComment(text)
-                console.log(text)
-                console.log(removeComment)
             }}>Remove Comment</button>
         </div>
     )
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Comment from './Comment'
 
 function CommentList({comments, removeComment}) {
+    const [searchText, setSearchText ] = useState('')
+    const [sort, setSort] = useState('AZ')
+    
+
+    let sortedComments = [...comments]
+    if (sort === 'AZ'){
+        sortedComments.sort()    
+    }
+    else if (sort === 'ZA'){
+        sortedComments.sort().reverse()
+    }
+
+
     return (
         <div>
             <h2>{comments.length} Comments</h2>
-            {comments.map((comment)=>{
-                return <Comment user= {comment.user} text= {comment.comment} id={comment.id} removeComment={removeComment}/>
+            <input type= 'text' placeholder='...Search by username' value={searchText} onChange={(e)=>{
+                setSearchText(e.target.value)
+            }}/>
+            <div>
+                <p>Sort By:</p>
+                <select onChange={
+                    (e)=>{
+                        setSort(e.target.value)
+                        console.log(sortedComments)
+                    }
+                }>
+                    <option value='AZ'>Username A-Z</option>
+                    <option value='ZA'>Username Z-A</option>
+                    {/* <option value='likes'>Likes</option> */}
+                </select>
+            </div>
+            
+            {sortedComments.filter((comment)=>comment.user.toLowerCase().includes(searchText.toLowerCase()))
+            .map((comment)=>{
+                return <Comment key = {comment.id} user= {comment.user} text= {comment.comment} id={comment.id} removeComment={removeComment} />
             })}
         </div>
     )
